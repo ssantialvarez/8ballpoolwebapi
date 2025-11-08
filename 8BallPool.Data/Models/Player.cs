31cf4e808807ffdcf73d72be0976e27c3cdd2b17
Santiago Alvarez <alvarezsanjavier@gmail.com>
2025-11-08 13:16:48 -0300

    CHANGE: adaptado el modelo player para manejar collection navigations

--- 8BallPool.Data/Models/Player.cs
+++ 8BallPool.Data/Models/Player.cs
@@ -1,4 +1,5 @@
 namespace _8BallPool.Data.Models;
+
 public class Player
 {
     public int Id { get; set; }
@@ -7,4 +8,9 @@ public class Player
     public int Ranking { get; set; }
     public string? Preferred_cue { get; set; }
     public required string Profile_picture_url { get; set; }
+    
+    // Collection navigations for matches
+    public ICollection<Match> MatchesAsPlayer1 { get; } = new List<Match>();
+    public ICollection<Match> MatchesAsPlayer2 { get; } = new List<Match>();
+    public ICollection<Match> MatchesWon { get; } = new List<Match>();
 }
